fbc1b90388a96eaac9ef9fe6e6f4b1fb47d30973
luis <luis.contreras@nexit.com.uy>
2021-11-15 22:03:45 -0300

    [IMP] amplia logica de scrapeo

--- app.py
+++ app.py
@@ -1,45 +1,65 @@
-from flask import Flask
-import requests
-import lxml.html as lh
-import pandas as pd
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    flash,
+    url_for,
+    current_app
+)
+
+import urllib.request
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
+import requests, validate, json, uuid, pathlib, os
 
 app = Flask(__name__)
 URL = 'https://www.ine.gub.uy/indicadores?indicadorCategoryId=42887'
 
 
-@app.route("/")
-def get_data():
-    page = requests.get(URL)
-    doc = lh.fromstring(page.content)
-    tr_elements = doc.xpath('//tr')
-    col = []
-    i = 0
-    for t in tr_elements[0]:
-        i += 1
-        name = t.text_content()
-        print('%d:"%s"' % (i, name))
-        col.append((name, []))
+@app.route("/", methods=("GET", "POST"), strict_slashes=False)
+def index():
+    if request.method == "POST":
+        try:
+            global request_url, specific_element, tag
+            request_url = request.form.get('urltext')
+            tag = request.form.get('SpecificElement')
+            source = request.get(request_url).text
+            soup = BeautifulSoup(source, "html.parser")
+            specific_element = soup.findall(tag)
+            counter = len(specific_element)
 
-    for j in range(1, len(tr_elements)):
-        # T is our j'th row
-        T = tr_elements[j]
-        # If row is not of size 10, the //tr data is not from our table
-        if len(T) != 10:
-            break
-        # i is the index of our column
-        i = 0
-        # Iterate through each element of the row
-        for t in T.iterchildren():
-            data = t.text_content()
-            # Check if row is empty
-            if i > 0:
-                # Convert any numerical value to integers
-                try:
-                    data = int(data)
-                except:
-                    pass
-            # Append the data to the empty list of the i'th column
-            col[i][1].append(data)
-            # Increment i for the next column
-            i += 1
-    return pd.DataFrame({title: column for (title, column) in col}).head()
+            image_paths = image_handler(
+                tag,
+                specific_element,
+                request_url
+            )
+
+            return render_template("index.html",
+                                   url=request_url,
+                                   counter=counter,
+                                   image_paths=image_paths,
+                                   results=specific_element)
+        except Exception as e:
+            flash(e, "danger")
+
+
+def image_handler(tag,specific_element,requested_url):
+    image_paths = []
+    if tag == 'img':
+        images = [img['src'] for img in specific_element]
+        for i in specific_element:
+            image_path = i.atrrs['src']
+            valid_imgpath = validate.url(image_path)
+            if valid_imgpath == True:
+                full_path = image_path
+            else:
+                full_path = urljoin(requested_url, image_path)
+                image_paths.append(full_path)
+    return image_paths
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
