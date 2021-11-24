348eac47c5f990b7e82545c7a2e23d31d76e6d63
luis <luis.contreras@nexit.com.uy>
2021-11-23 21:25:21 -0300

    [wip] obtiene el texto de todos los td aun sin ordenar.

--- app.py
+++ app.py
@@ -19,46 +19,23 @@ URL = 'https://www.ine.gub.uy/indicadores?indicadorCategoryId=42887'
 
 @app.route("/", methods=("GET", "POST"), strict_slashes=False)
 def index():
-    if request.method == "POST":
-        try:
-            global request_url, specific_element, tag
-            request_url = request.form.get('urltext')
-            tag = request.form.get('SpecificElement')
-            source = request.get(request_url).text
-            soup = BeautifulSoup(source, "html.parser")
-            specific_element = soup.findall(tag)
-            counter = len(specific_element)
-
-            image_paths = image_handler(
-                tag,
-                specific_element,
-                request_url
-            )
-
-            return render_template("index.html",
-                                   url=request_url,
-                                   counter=counter,
-                                   image_paths=image_paths,
-                                   results=specific_element)
-        except Exception as e:
-            flash(e, "danger")
+    data_lists = []
+    if requests.get(URL):
+        html_content = requests.get(URL).text
+        soup = BeautifulSoup(html_content, "lxml")
+        rows = soup.find_all("tr")
+        count = len(rows)
+        for row in rows:
+            print('{}'.format(row.find_all("td")))
+            cell = row.find_all("td")
+            data_lists.append([row.get_text('//')])
+            count = count - 1
+        return test_template(data_lists)
 
 
-def image_handler(tag,specific_element,requested_url):
-    image_paths = []
-    if tag == 'img':
-        images = [img['src'] for img in specific_element]
-        for i in specific_element:
-            image_path = i.atrrs['src']
-            valid_imgpath = validate.url(image_path)
-            if valid_imgpath == True:
-                full_path = image_path
-            else:
-                full_path = urljoin(requested_url, image_path)
-                image_paths.append(full_path)
-    return image_paths
-
-
+@app.route('/test_template/')
+def test_template(table):
+    return render_template('index.html', table=table)
 
 
 if __name__ == "__main__":
